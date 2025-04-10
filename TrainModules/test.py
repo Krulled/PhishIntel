8a08ef71c170b383a22a848527499748a90ba0a9
Zach <zachxhobbs@gmail.com>
2025-04-10 14:25:10 -0700

    Finished Decision Tree ML Model, Exteranl VT API check as well, features.py is done

--- TrainModules/test.py
+++ TrainModules/test.py
@@ -1,12 +1,41 @@
-import pandas as pd
-from sklearn.datasets import load_breast_cancer
-data = load_breast_cancer()
-dataset = pd.DataFrame(data=data['data'], columns=data['feature_names'])
+import vt
+client = vt.Client("54ec36e901c1901677338551c4ebae5f350da2f7a85877d939e84c70eb9f47d4")
+   #Use VT API to pull score, and extra reasons 
 
-from sklearn.model_selection import train_test_split
-X = dataset.copy()
-y = data['target']
-target_counts = pd.Series(y).value_counts()
+analysis = client.scan_url("http://168.99.76.43/?rid=LHCrKai")
 
-print(target_counts)
-print(X)
+while True:
+  analysis = client.scan_url("http://168.99.76.43/?rid=LHCrKai", analysis.id)
+  print(analysis.status)
+  if analysis.status == "completed":
+     break
+  time.sleep(30)
+
+
+
+stats = analysis.get('stats', {})
+malicious_count = stats.get('malicious', 0)
+total_engines = sum(stats.values())
+
+print(f"Community Score: {malicious_count} out of {total_engines}")
+
+# Now iterate through the results to get the details for vendors marking the URL as malicious.
+results = analysis.get('results', {})
+count = 0
+
+print("\nDetails of vendors that marked the URL as malicious:")
+for vendor, data in results.items():
+    # Check if the vendor flagged the URL as malicious
+    if data.get('category') == 'malicious' or data.get('result') in ['malware', 'phishing']:
+        print(f"\nVendor: {vendor}")
+        print(f"  Engine Name: {data.get('engine_name')}")
+        print(f"  Method: {data.get('method')}")
+        print(f"  Category: {data.get('category')}")
+        print(f"  Result: {data.get('result')}")
+        if data.get('result') == 'phishing':
+        	count += 1
+
+
+print(count)
+client.close()  
+
